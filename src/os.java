501ce73e02e0b37b6d53edbc8256250b0be0455d
Lancetl <lancetleonard@hotmail.com>
2015-11-08 15:01:06 -0500

    Kwasi added

--- src/os.java
+++ src/os.java
@@ -1,71 +1,88 @@
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.Hashtable;
+
 
 public class os {
+		
+	/*
+	 * ArrayList to hold out PCB
+	 * AL was chosen jobs do not have to be in order
+	 * thus we can stick everything in the end for O(1) amortized time
+	 * subject to change
+	 */
+	// public static Vector<Vector<Integer>> JobTable = new Vector<Vector<Integer>>(50);
+	 public static ArrayList<int[]> JobTable=new ArrayList<int[]>(50);
+	 
+	 
+	 /*
+	  * array for memory management;
+	  */
+	 
+	 public static Vector<Integer> memory = new Vector(50);
+	 
+	 //freeSpaceTable implementation using hashtable
+	 
+	 public static Hashtable<Integer, Integer>FreeSpaceTable = new Hashtable<Integer, Integer>();
+	 public static int memoryleft=100;
+	 
 
-	
-	public static void startup()
-	{
-		
+	public static void startup(){
+		//sos.ontrace();
+		System.out.print("TEEEEEESSSSSSSSSTTTTTTT");
+
 	}
 	
-    public void siodisk(int jobnum)
-	{
+	//Interrupt handlers
+	
+
+	/*
+	 * indicates the arrival of a new job on drum
+	 * puts job onto JobTable (vector of vectors)
+	 * numofJobs- global variable used to access job
+	 * 
+	 */
+	public static void Crint (int []a, int[] p){
+		
+		JobTable.add(p);
+		a[0]= 2;
+		MemoryManager(p);
+	}
+
+	public static void Dskint (int []a, int []p){
 		
 	}
-	public void siodrum(int jobnum, int jobsize, int coreaddress, int direction)
-	{
+
+	public static void Drmint (int []a, int []p){
 		
 	}
-	/*public void ontrace()
-	{
+
+	public static void Tro (int []a, int []p){
 		
 	}
-	*/
-	public void offtrace()
-	{
+
+	public static void Svc (int []a, int []p){
 		
-	}
-	//interrupt Handlers
-	public static void Crint(int []a, int[] p)
-	{
-		/*indicates the arrival of a new job on the drum
-		 *at call: p[0] = job number
-		 *p[1] = priority
-		 *p[2] = job,size
-		 *p[3] = max CPU time allowed for job
-		 *p[4] = current time 
-		 */
-	}
-	public void Dskint(int []a, int[]p)
-	{
-		/*Disk interrupt
-		 * at call: p[4] = current time
-		 * 
-		 */
-		
-	}
-	public void Drmint(int []a, int []p)
-	{
-		/*
-		 * Drum interrupt
-		 * At call: p[4] = current time
-		 */
-	}
-	public void Tro(int []a, int[]p)
-	{
-		/*
-		 * Timer-run-out
-		 * At Call :p[4] = current time
-		 */
-	}
-	public void Svc(int  a, int[]p)
-	{
-		/*Supervisor call from user program
-		 * At call p[4] = current time
-		 * a = 5 => job has terminated
-		 * a = 6 => job request disk i/o
-		 * a = 7 => job wants to be blocked until all its pending
-		 * i/o requests are completed
-		 */
 	}
 	
+	//methods called by OS only
+	
+	public static void MemoryManager( int []p){
+	
+		if(p[3]<memoryleft){
+			int count =p[3];
+			while(count !=0){
+			memory.add(p[1]);
+			count--;
+			memoryleft--;
+			Swap(p);
+			}
+		}
+	
+
+	}
+	
+	public static void Swap(int[] p){
+		sos.siodrum(p[1], 0, p[3], 9);
+	}
 }
