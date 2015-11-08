351e6d7afb8c7abcd739970c24caf4e3669a31a4
Lancetl <lancetleonard@hotmail.com>
2015-11-08 13:49:50 -0500

    Added Functions to the os Class

--- src/os.java
+++ src/os.java
@@ -1,4 +1,71 @@
 
 public class os {
 
+	
+	public static void startup()
+	{
+		
+	}
+	
+    public void siodisk(int jobnum)
+	{
+		
+	}
+	public void siodrum(int jobnum, int jobsize, int coreaddress, int direction)
+	{
+		
+	}
+	/*public void ontrace()
+	{
+		
+	}
+	*/
+	public void offtrace()
+	{
+		
+	}
+	//interrupt Handlers
+	public static void Crint(int a, int[] p)
+	{
+		/*indicates the arrival of a new job on the drum
+		 *at call: p[0] = job number
+		 *p[1] = priority
+		 *p[2] = job,size
+		 *p[3] = max CPU time allowed for job
+		 *p[4] = current time 
+		 */
+	}
+	public void Dskint(int a, int[]p)
+	{
+		/*Disk interrupt
+		 * at call: p[4] = current time
+		 * 
+		 */
+		
+	}
+	public void Drmint(int a, int []p)
+	{
+		/*
+		 * Drum interrupt
+		 * At call: p[4] = current time
+		 */
+	}
+	public void Tro(int a, int[]p)
+	{
+		/*
+		 * Timer-run-out
+		 * At Call :p[4] = current time
+		 */
+	}
+	public void Svc(int  a, int[]p)
+	{
+		/*Supervisor call from user program
+		 * At call p[4] = current time
+		 * a = 5 => job has terminated
+		 * a = 6 => job request disk i/o
+		 * a = 7 => job wants to be blocked until all its pending
+		 * i/o requests are completed
+		 */
+	}
+	
 }
