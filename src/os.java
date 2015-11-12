f9af29245ea3f5ffb892d8442c2076e120aba6c2
kamanfi <kamanfi@gmail.com>
2015-11-12 01:27:10 -0500

    testupdate

--- src/os.java
+++ src/os.java
@@ -13,8 +13,9 @@ public class os {
 	 */
 	// public static Vector<Vector<Integer>> JobTable = new Vector<Vector<Integer>>(50);
 	 public static ArrayList<int[]> JobTable=new ArrayList<int[]>(50);
-	 
-	 
+	 public static int[] mask = new int [50];
+	 public static int index=0;
+	 static int f =0;
 	 /*
 	  * array for memory management;
 	  */
@@ -23,12 +24,12 @@ public class os {
 	 
 	 //freeSpaceTable implementation using hashtable
 	 
-	 public static Hashtable<Integer, Integer>FreeSpaceTable = new Hashtable<Integer, Integer>();
+	// public static Hashtable<Integer, Integer>FreeSpaceTable = new Hashtable<Integer, Integer>();
 	 public static int memoryleft=100;
 	 
 
 	public static void startup(){
-		//sos.ontrace();
+		sos.ontrace();
 		System.out.print("gittest");
 
 	}
@@ -38,51 +39,63 @@ public class os {
 
 	/*
 	 * indicates the arrival of a new job on drum
-	 * puts job onto JobTable (vector of vectors)
-	 * numofJobs- global variable used to access job
-	 * 
+	 * puts job onto JobTable 
+	 * mask is used to check the index  in arraylist 
+	 * a job stored(
 	 */
 	public static void Crint (int []a, int[] p){
 		
 		JobTable.add(p);
-		a[0]= 2;
+		mask[index]=p[3];
+		index++;
+		a[0]= 1;
 		MemoryManager(p);
 	}
 
 	public static void Dskint (int []a, int []p){
 		
+		a[0]=2;
+		p[2]=0;
+		f+=20;
 	}
 
 	public static void Drmint (int []a, int []p){
+		
+		a[0]=2;
+		p[2]=0;
 		
 	}
 
 	public static void Tro (int []a, int []p){
 		
+		a[0]=2;
+		p[2]=0;
 	}
 
 	public static void Svc (int []a, int []p){
+		if(a[0] == 5)
+			;
+		else 
+			if(a[0] == 6)
+				sos.siodisk(p[1]);
+			
+			else
+				if(a[0] == 7)
+					sos.siodisk(p[1]);
+					a[0]=2;
 		
 	}
 	
 	//methods called by OS only
 	
 	public static void MemoryManager( int []p){
-	
-		if(p[3]<memoryleft){
-			int count =p[3];
-			while(count !=0){
-			memory.add(p[1]);
-			count--;
-			memoryleft--;
-			Swap(p);
-			}
-		}
-	
 
-	}
+			Swap(p,0);
+			
+		}
+
 	
-	public static void Swap(int[] p){
-		sos.siodrum(p[1], 0, p[3], 9);
+	public static void Swap(int[] p, int k){
+		sos.siodrum(p[1], p[3], 0, 0);
 	}
 }
