6bc61a0b3dcac963ac85136e908fde9cf8a59be1
kamanfi <kamanfi@gmail.com>
2015-11-19 21:26:43 -0500

    working on 3

--- src/cpuScheduler.java
+++ src/cpuScheduler.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 
 public class  cpuScheduler {
 	
-	int timeslice=5;
+	static int timeslice=5;
 
 	public static void roundRobin(ArrayList jobTable,int p[],int index){
 				job job;
 				job =(job) jobTable.get(index);
 				int max=job.getMax();
 				if (max>5){
-					p[4]=5;
-					job.setMax(max-=5);
+					p[4]=timeslice;
+					job.setMax(max-=timeslice);
 				}
 					else
 						p[4]=max;
