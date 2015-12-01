1849fc97215d1afe24e0a14ae22bde45c5b30b28
kwasi <M196@146.245.190.222>
2015-12-01 14:32:24 -0500

    4/5

--- src/job.java
+++ src/job.java
@@ -6,12 +6,33 @@ public class job {
 	private int max;
 	private int entered;
 	
+	//variables needed to keep track of time
+	private int timeOnCpu;
+	private int absMax;
+	private int cputracker;
+	
+	//variables needed to memoryLink
+	private int address;
+	private boolean freeSpace;
+	private boolean latched;
+	
 	public job(int jobID, int prio, int size, int max, int entered){
 		this.jobID = jobID;
 		this.prio = prio;
 		this.size = size;
 		this.max = max;
 		this.entered = entered;
+		this.latched= false;
+		this.timeOnCpu = 0;
+		this.absMax= max;
+		this.cputracker = 0;
+	}
+	public job(int jobID, int size, int address, boolean freeSpace, boolean latched){
+		this.jobID = jobID;
+		this.size = size;
+		this.address = address;
+		this.freeSpace = freeSpace;
+		this.latched = latched;
 	}
 
 	public int getJobID() {
@@ -53,6 +74,44 @@ public class job {
 	public void setEntered(int entered) {
 		this.entered = entered;
 	}
-
-
+	public int getAddress(){
+		return address;
+	}
+	public void setAddress(int address){
+		this.address = address;
+	}
+	public boolean getFreeSpace(){
+		return freeSpace;
+	}
+	public void setFreeSpace(boolean freeSpace){
+		this.freeSpace = freeSpace;
+	}
+	public boolean getLatched(){
+		return latched;
+	}
+	public void setLatched(boolean latched){
+		this.latched = latched;
+	}
+	public void subSize(int size){
+		this.size -= size;
+	}
+	public int getTimeOnCpu() {
+		return timeOnCpu;
+	}
+	public void setTimeOnCpu(int timeOnCpu) {
+		this.timeOnCpu = timeOnCpu;
+	}
+	public int getAbsMax() {
+		return absMax;
+	}
+	public void setAbsMax(int absMax) {
+		this.absMax = absMax;
+	}
+	public int getCputracker() {
+		return cputracker;
+	}
+	public void setCputracker(int cputracker) {
+		this.cputracker = cputracker;
+	}
+	
 }
--- src/os.java
+++ src/os.java
@@ -17,6 +17,7 @@ public class os {
 	 public static int BAfreespace=0;//baseaddress of freespace
 	 public static int trans=0;
 	 public static boolean iopending=false;
+	 public static memoryLink memoryLink;
 	 /*
 	  * array for memory management;
 	  */ 
@@ -25,6 +26,7 @@ public class os {
 
 	public static void startup(){
 		sos.ontrace();
+		memoryLink = new memoryLink();
 		System.out.print("Long live our idols");
 
 	}
@@ -40,62 +42,87 @@ public class os {
 	 */
 	public static void Crint (int []a, int[] p){
 		
-		job job = new job(p[1],p[2],p[3],p[4],p[5]);// create new job object
-		jobTable.add(index,job);// adds job to JobTable
-		index++;// increase index to where next job is going to coming in
-				
-		Swap(p,0); //job is swapped		
+		
+		MemoryManager(p);	
+		//Swap(p,0); //job is swapped		
 		a[0]=1;
 
 	}
 
 	public static void Dskint (int []a, int []p){
-		iopending=false;
-		
+		trackTime(p,1);
+		doIo(p,false);
+		System.out.println(print(p));
+		System.out.println("MAKUTA MATATA");
 	}
 
 	/*
 	 * Drum interrup: transfer between Drum and memory is done
 	 */
 	public static void Drmint (int []a, int []p){
+		
 		if(trans == 0){//if it is in memory
+			trackTime(p,1);
 			cpuScheduler.roundRobin(jobTable,p,index-1);
 			a[0]=2;
-			p[2]=BAfreespace;	
+			p[2]=memoryLink.addressFinder(p[3]);
 			}
-		else
+		else{
 			a[0]=1;
-	}
+		}
+		System.out.println(print(p));
+		System.out.println("MAKUTA 5454 MATATA");
+		}
 
 	public static void Tro (int []a, int []p){
-			
+		
+		if(timeup(p) == false){
+			trackTime(p,0);
+			trackTime(p,1);
+			if(timeup(p) == false){
 			cpuScheduler.roundRobin(jobTable,p,index-1);
 			a[0]=2;
-	
-		
+			System.out.println(print(p));
+			System.out.println("MAKUTA MATATA");
+			}
+			else{
+				a[0]=1;
+			}
+		}
+			else{
+			a[0]=1;
+			}
 	}
+	
 
 	public static void Svc (int []a, int []p){
-		
 		if(a[0] == 5){
+			//trackTime(p,0);
 			a[0]=1;
 			jobTable.remove(p);
+			System.out.println(print(p));
+			System.out.println("MAKUTA MATATA");
 		}
 		else 
 			if(a[0] == 6){
-				iopending=true;
-				cpuScheduler.roundRobin(jobTable,p,index-1);
+				doIo(p,true);
+				cpuScheduler.roundRobin(jobTable,p,index-1);//gives time quantum
 				sos.siodisk(p[1]);
 				a[0]=2;
+				System.out.println(print(p));
+				System.out.println("MAKUTA MATATA");
 			}
 			else
 				if(a[0] == 7){
-					if(iopending == true)
+					trackTime(p,0);
+					if(doingIo(p) == true)
 					a[0]=1;
 					else{
 						System.out.print("MAY THEY NEVER BE OUR RIVAL");
-						a[0]=2;
+						a[0]=2; 
 					}
+				System.out.println(print(p));
+				System.out.println("MAKUTA MATATA");
 				}
 	}
 
@@ -109,14 +136,26 @@ public class os {
 	 * and returns true
 	 * if not it returns false
 	 */
-	public  static boolean  MemoryManager(  int[] p){
+	public static void MemoryManager(int[] p){
+		
+			memoryLink.freeSpaceTableBuilder();
+			p[2] = memoryLink.addressFinder(p[3]);
+			memoryLink.addTooMemory(p[2],p[3],p[1]);
+			memoryLink.merge();
 			
+			job job = new job(p[1],p[2],p[3],p[4],p[5]);// create new job object
+			jobTable.add(index,job);// adds job to JobTable
+			index++;// increase index to where next job is going to coming in
+			
+			Swap(p,0);
+				/*
 			if(firstFit(p[3]) == true){
 			Swap(p,0);
 			return true;
 			}
 			else 
-				return false;			
+				return false;
+				*/			
 		}
 
 	// swap job in or out of memory
@@ -135,5 +174,54 @@ public class os {
 		 //subtract size from memory
 		return true;
 		
+	}
+	
+	public static void doIo(int[] p, boolean val){
+		for(job temp : jobTable){
+			if(temp.getJobID() == p[1])
+				temp.setLatched(val);
+		}
+	}
+	
+	public static boolean doingIo(int[] p){
+		for(job temp : jobTable){
+			if(temp.getJobID() == p[1])
+				return temp.getLatched();
+		}
+		return false;
+	}
+	
+	public static void trackTime(int[] p, int flag){
+		int time=0;
+		for(job temp : jobTable){
+			if(temp.getJobID() == p[1]){
+				if(flag == 1){
+					temp.setCputracker(p[5]);
+				}
+				else{
+					time = temp.getTimeOnCpu();
+					 temp.setTimeOnCpu(time+(p[5]-temp.getCputracker()) );
+				}
+				}
+		}
+	}
+	//if mmaxtime exceeds xpu time
+	public static boolean timeup(int[] p){
+		for(job temp : jobTable){
+			if(temp.getJobID() == p[1]){
+				if(temp.getTimeOnCpu() >= temp.getAbsMax())
+					return true;
+			}
+		}
+		return false;
+	}
+	public static int print(int[] p){
+		
+		for(job temp : jobTable){
+			if(temp.getJobID() == p[1]){
+				return temp.getTimeOnCpu();
+			}
+		}
+		return 0;
 	}
 }
